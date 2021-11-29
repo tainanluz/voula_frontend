1fa466ed39359609f86563019aff8d494df5a34e
unknown <contato.tainanluz@gmail.com>
2021-11-28 21:25:33 -0300

    Final ajustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComecarComponent } from './comecar/comecar.component';
+import { LocaisComponent } from './locais/locais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'', redirectTo:'body', pathMatch: 'full'},
+{path:'locais' , component:LocaisComponent},
+{path:'comecar', component:ComecarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
